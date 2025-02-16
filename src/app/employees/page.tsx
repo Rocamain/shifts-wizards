016fe59c39f30d587680d547fe6a0285f920fc6a
Rocamain <fjrocavazquez@gmail.com>
2025-02-16 15:52:45 +0000

    Implements EmployeeTable component

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+import { useState } from "react";
+import EmployeeTable from "@/ui/Employees/EmployeeTable";
+import { EMPLOYEES } from "@/lib/employees/constants";
+
 export default function EmpoloyeePage() {
+  const [employees, setEmployees] = useState(() => EMPLOYEES);
+
+  const handleRemoveEmployee = (employeeId: string) => {
+    setEmployees((prev) =>
+      prev.filter((employee) => employee.id !== employeeId)
+    );
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-4">
-      <div className="max-w-7xl mx-auto">Empoloyee Page</div>
+      <div className="max-w-7xl mx-auto">
+        <EmployeeTable
+          employees={employees}
+          onRemoveEmployee={handleRemoveEmployee}
+        />
+      </div>
     </div>
   );
 }
