4e233c8f3d7b698e9c4a65e9865ca07af4db7455
Rocamain <fjrocavazquez@gmail.com>
2025-06-12 09:17:37 +0100

    Refactor RouteSelector: use Box wrapper and include Baker and Full rota links

--- src/ui/Rota/RouteSelector.tsx
+++ src/ui/Rota/RouteSelector.tsx
@@ -1,19 +1,14 @@
-import NavLink from "./NavLink";
-
+import NavLink from "../Other/NavLink";
+import Box from "../Other/Box";
 export default function RouteSelector({}) {
   return (
-    <div>
-      <div className="flex justify-end bg-gray-200">
-        <div className="relative bg-gray-200 border border-gray-500 rounded px-7 py-6">
-          <div className="absolute -top-3 left-1/2 transform -translate-x-1/2 bg-gray-200 text-center px-2 text-gray-600">
-            <h3 className="font-semibold bg-gray-200"> Choose a Role </h3>
-          </div>
-          <nav className="flex justify-center space-x-6">
-            <NavLink href={"/rota/tl"}>TL Rota</NavLink>
-            <NavLink href={"/rota/ctm"}>CTM Rota</NavLink>
-          </nav>
-        </div>
-      </div>
-    </div>
+    <Box title="Choose a Rota">
+      <nav className="flex justify-center space-x-6">
+        <NavLink href={"/rota/tl"}>TL Rota</NavLink>
+        <NavLink href={"/rota/ctm"}>CTM Rota</NavLink>
+        <NavLink href={"/rota/baker"}>Baker Rota</NavLink>
+        <NavLink href={"/rota/full"}>Full Rota</NavLink>
+      </nav>
+    </Box>
   );
 }
