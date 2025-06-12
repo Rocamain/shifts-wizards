b8066eafccd0b197a3ca4656f268372fe0c94f42
Rocamain <fjrocavazquez@gmail.com>
2025-06-12 09:07:37 +0100

    Refactor layouts: simplify RootLayout and Rota layouts; add role-specific layout structure

--- src/app/rota/layout.tsx
+++ src/app/rota/layout.tsx
@@ -1,27 +1,18 @@
-import RouteSelector from "@/ui/Rota/RouteSelector";
-import ShopHours from "@/ui/Rota/ShopHours";
-
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: `Shift Wizard Create Rota Main Page`,
   description: "Create your rota for the week",
 };
-type Params = Promise<{ role: "CTM" | "TL" }>;
 
-export default function Layout({
+export default function LayoutRota({
   children,
 }: {
   children: React.ReactNode;
-  params: Params;
 }) {
   return (
     <div className="mt-10 bg-gray-200">
       <h2 className="text-2xl font-semibold">Shift Wizard Rota</h2>
-      <div className="py-8 flex justify-between items-center">
-        <ShopHours />
-        <RouteSelector />
-      </div>
 
       {children}
     </div>
--- src/app/rota/page.tsx
+++ src/app/rota/page.tsx
@@ -1,3 +1,9 @@
+import RouteSelector from "@/ui/Rota/RouteSelector";
+
 export default function Rota() {
-  return null;
+  return (
+    <div className="py-8 flex justify-between items-center  flex-wrap">
+      <RouteSelector />
+    </div>
+  );
 }
